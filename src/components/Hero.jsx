aa0e0ba20867531e4327f9593cf4c4470a9a755c
Pratham <udeshipratham@gmail.com>
2024-05-02 09:38:59 +0530

    Temporary 3D model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {motion} from 'framer-motion';
 import {styles} from '../styles';
 import{ComputersCanvas} from './canvas';
+import { INFINITY } from 'three/examples/jsm/nodes/Nodes.js';
 
 const Hero = () => {
   return (
@@ -13,8 +14,27 @@ const Hero = () => {
         </div>
         <div>
           <h1 className = {`${styles.heroHeadText} text-white`}>Hi I'm <span className = "text-[#915eff]"> Pratham</span></h1>
-          <p className = {`${styles.heroSubText} mt-2 text-white-100`}></p>
+          <p className = {`${styles.heroSubText} mt-2 text-white-100`}>I Develop 3D Visuals, user<br className = "sm:block hidden" /> interfaces and web applications</p>
         </div>
+
+      </div>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev 
+              animate = {{
+                y:[0,24,0]
+              }}
+              transition = {{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop"
+              }}
+              className = "w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   )
