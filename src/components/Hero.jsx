c36fe70bf4cc6b2245ec9417b2129309debea6b4
Pratham <udeshipratham@gmail.com>
2024-05-22 22:42:38 +0530

    scroller trial 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
 
       </div>
       <ComputersCanvas />
-      <div className="absolute bottom-10 sm:bottom-10  w-full flex justify-center items-center">
+      <div className="absolute bottom-14 sm:bottom-10  w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev 
